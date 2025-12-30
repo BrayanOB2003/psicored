57a4ea2c3045b0931e4639bac54e133657d408b3
neajmorshed0 <neajmorshedimon@gmail.com>
2025-12-30 18:10:42 +0600

    update date picker

--- src/components/ecommerce/StatisticsChart.vue
+++ src/components/ecommerce/StatisticsChart.vue
@@ -10,21 +10,53 @@
         </p>
       </div>
 
-      <div class="relative">
-        <div class="inline-flex items-center gap-0.5 rounded-lg bg-gray-100 p-0.5 dark:bg-gray-900">
-          <button
-            v-for="option in options"
-            :key="option.value"
-            @click="selected = option.value"
-            :class="[
-              selected === option.value
-                ? 'shadow-theme-xs text-gray-900 dark:text-white bg-white dark:bg-gray-800'
-                : 'text-gray-500 dark:text-gray-400',
-              'px-3 py-2 font-medium rounded-md text-theme-sm hover:text-gray-900 hover:shadow-theme-xs dark:hover:bg-gray-800 dark:hover:text-white',
-            ]"
+      <div class="flex items-center gap-3">
+        <div class="relative">
+          <div
+            class="inline-flex items-center gap-0.5 rounded-lg bg-gray-100 p-0.5 dark:bg-gray-900"
           >
-            {{ option.label }}
-          </button>
+            <button
+              v-for="option in options"
+              :key="option.value"
+              @click="selected = option.value"
+              :class="[
+                selected === option.value
+                  ? 'shadow-theme-xs text-gray-900 dark:text-white bg-white dark:bg-gray-800'
+                  : 'text-gray-500 dark:text-gray-400',
+                'px-3 py-2 font-medium rounded-md text-theme-sm hover:text-gray-900 hover:shadow-theme-xs dark:hover:bg-gray-800 dark:hover:text-white',
+              ]"
+            >
+              {{ option.label }}
+            </button>
+          </div>
+        </div>
+
+        <div class="relative">
+          <flat-pickr
+            v-model="date"
+            :config="flatpickrConfig"
+            class="pl-3 sm:pl-9 dark:bg-dark-900 h-10 w-10 sm:w-40 rounded-lg border border-gray-200 bg-white text-transparent sm:text-theme-sm sm:text-gray-800 shadow-theme-xs placeholder:text-transparent sm:placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-white/[0.03] dark:text-transparent sm:dark:text-gray-400 dark:placeholder:text-transparent sm:dark:placeholder:text-gray-400 dark:focus:border-brand-800"
+            placeholder="Select Date"
+          />
+          <span
+            class="absolute text-gray-500 -translate-y-1/2 pointer-events-none left-1/2 -translate-x-1/2 sm:left-3 sm:translate-x-0 top-1/2 dark:text-gray-400"
+          >
+            <svg
+              class="fill-current"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.66659 1.5415C7.0808 1.5415 7.41658 1.87729 7.41658 2.2915V2.99984H12.5833V2.2915C12.5833 1.87729 12.919 1.5415 13.3333 1.5415C13.7475 1.5415 14.0833 1.87729 14.0833 2.2915V2.99984L15.4166 2.99984C16.5212 2.99984 17.4166 3.89527 17.4166 4.99984V7.49984V15.8332C17.4166 16.9377 16.5212 17.8332 15.4166 17.8332H4.58325C3.47868 17.8332 2.58325 16.9377 2.58325 15.8332V7.49984V4.99984C2.58325 3.89527 3.47868 2.99984 4.58325 2.99984L5.91659 2.99984V2.2915C5.91659 1.87729 6.25237 1.5415 6.66659 1.5415ZM6.66659 4.49984H4.58325C4.30711 4.49984 4.08325 4.7237 4.08325 4.99984V6.74984H15.9166V4.99984C15.9166 4.7237 15.6927 4.49984 15.4166 4.49984H13.3333H6.66659ZM15.9166 8.24984H4.08325V15.8332C4.08325 16.1093 4.30711 16.3332 4.58325 16.3332H15.4166C15.6927 16.3332 15.9166 16.1093 15.9166 15.8332V8.24984Z"
+                fill=""
+              />
+            </svg>
+          </span>
         </div>
       </div>
     </div>
@@ -36,8 +68,9 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
+import flatPickr from 'vue-flatpickr-component'
 
 const options = [
   { value: 'optionOne', label: 'Monthly' },
@@ -46,6 +79,13 @@ const options = [
 ]
 
 const selected = ref('optionOne')
+const date = ref('')
+
+const flatpickrConfig = {
+  mode: 'range',
+  dateFormat: 'M j',
+  defaultDate: [new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), new Date()],
+}
 import VueApexCharts from 'vue3-apexcharts'
 
 const series = ref([
