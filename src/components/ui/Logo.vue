9dc129a0c95ffe9eb1e2fef8bb7aedd166f21498
Brayan Ortiz <brayanortiz@mejorcdt.com>
2026-01-17 19:27:45 -0500

    Refactor Signin view and add logo component; update package-lock and main.css

---
+++ src/components/ui/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-row items-center max-w-xs">
+        <img class="mb-4" width="150" height="48" src="/images/logo/logo.png" alt="Logo" />
+        <div class="ml-3 text-8xl font-bold text-white font-mplus-rounded mb-4">
+            Vita
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
